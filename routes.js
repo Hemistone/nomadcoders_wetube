3173dcd4e82b8725807bdb80adae213736849e56
Pansok Hwang <kei6040@gmail.com>
2019-04-04 22:53:50 +0900

    took lecture untill 2.10 - MVC part2

---
+++ routes.js
@@ -0,0 +1,13 @@
+// Global
+
+const HOME = "/";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+const SEARCH = "/search";
+
+// Users
+
+const USER = "/users";
+const USER_DETAIL = "/:id";
+const USER_
