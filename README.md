be32ca528a68cc799efb6d276b954f7e57c6e64b
Pansok Hwang <kei6040@gmail.com>
2019-04-06 23:38:05 +0900

    took lecture ~2.22 (nomad coders - wetube)

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # nomadcoders_wetube
 a clone site of wetube which is a paid course of nomad coders. Thx Nicolas!
+
+## Pages:
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import { localsMiddleware } from "./middlewares";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
@@ -8,19 +9,20 @@ import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
+
 const app = express();
 
-app.set("view engine", "pug");
-
 //middleware
+app.use(helmet()); //For Secure
+app.set("view engine", "pug");
 app.use(cookieParser()); //check cookies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); //checks json or form elements in body
-app.use(helmet()); //For Secure
 app.use(morgan("dev")); //log all histories
 
+app.use(localsMiddleware);
 
-app.use(routes.home, globalRouter)
+app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 
