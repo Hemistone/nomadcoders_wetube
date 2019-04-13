bf5f741dda3790a8a336bdb2191ce3d2c3f8f34d
Pansok Hwang <kei6040@gmail.com>
2019-04-13 13:14:24 +0900

    latest commit

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 import express from "express";
 import morgan from "morgan";
-import { localsMiddleware } from "./middlewares";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
+import "./passport";
 
 const app = express();
+
+const CookieStore = MongoStore(session);
 
 //middleware
 app.use(helmet()); //For Secure
@@ -22,6 +29,14 @@ app.use(cookieParser()); //check cookies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); //checks json or form elements in body
 app.use(morgan("dev")); //log all histories
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CookieStore({ mongooseConnection: mongoose.connection })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(localsMiddleware);
 
--- init.js
+++ init.js
@@ -1,10 +1,12 @@
-import app from "./app";
 import "./db";
 import dotenv from "dotenv";
+import app from "./app";
+
 dotenv.config();
+
 import "./models/video";
 import "./models/comment";
-
+import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
